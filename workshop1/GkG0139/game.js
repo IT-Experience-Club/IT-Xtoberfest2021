921ffd56426b74b0acd0fd6ed779fb155bc69798
Koranat <47288.koranat@gmail.com>
2021-10-23 16:12:37 +0700

    Update code

--- workshop1/GkG0139/game.js
+++ workshop1/GkG0139/game.js
@@ -3,11 +3,9 @@ function play() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data.meals[0].strMeal)
-      var element = document.getElementById("cookie1");
-      element.classList.add("cookie-hide");
-      var element = document.getElementById("cookie2");
-      element.classList.add("cookie-show");
-      document.getElementById("fortune").innerHTML = data.meals[0].strMeal;
+      cookie1.classList.add("cookie-hide");
+      cookie2.classList.add("cookie-show");
+      fortune.innerHTML = data.meals[0].strMeal;
     })
     .catch((error) => console.log(error));
 }
