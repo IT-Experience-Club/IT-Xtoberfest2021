9047770f906daa593ec3d99aa111d2cd51363a43
Baipor <baipor2545@hotmail.com>
2021-10-30 21:46:25 +0700

    Update test.py
    Add a function in test.py of free project
    
    --Chartnayodom--

--- free project/test.py
+++ free project/test.py
@@ -1,2 +1,10 @@
 """main"""
+def greeting():
+    """Greeting function"""
+    ans = input("What's your name?")
+    print("Oh! your name is %s" %ans)
+    age = int(input("Then how old are you?"))
+    print("%d years old(s)" %age)
+    print("Nice to meet ya!!!!!")
 print("test")
+greeting()
